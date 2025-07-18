975368fe7f6fbb085cf2d5562dbd1ee991c8dc00
mindphil <philipumeadi@icloud.com>
2025-07-17 20:56:04 -0400

    dp

--- 401K/data_process.py
+++ 401K/data_process.py
@@ -12,11 +12,11 @@ def load_rdata(filepath):
     # 401k offering, 401k participation and net financial assets.
     # Maybe I extract the means of these variables, and use that for the df in the notebook to better represent the statistics?
 
-def whats_important(): #parameter should be df or filepath?
+#def whats_important(): #parameter should be df or filepath?
     #loop to iterate over all rows, find e401, p401, net_tfa, scrap the rest
     #work in 'describe' to get sample means, ect.
      
 
 
 # & then descriptive statistics and visualizations
-# I need to determine what is really important.
+# I need to determine what is really important
