eada1bcba66c32b7e39c41a9550d65d73304a299
mindphil <philipumeadi@icloud.com>
2025-07-26 17:11:06 -0400

    more hosue keeping

--- 401K/sipp1991data_process.py
+++ 401K/sipp1991data_process.py
@@ -1,8 +1,16 @@
 import pyreadr
 import pandas as pd
 
-#Using pyreader to read the RData file into pd df
+#Read the R data file into pd df
 
 def transform(filepath):
-    transformed = pyreadr.read_r(filepath)
-    return transformed
+    # Read the R data file - returns OrderedDict
+    r_data = pyreadr.read_r(filepath)
+    print("Available datasets in R file:")
+    for key in r_data.keys():
+        print(f"  - {key}: shape {r_data[key].shape}")
+        first_key = list(r_data.keys())[0]
+    return r_data[first_key]
+
+# Option 2: If you know the specific dataset name, use it directly
+    # return r_data['sipp1991']  # Replace with actual dataset name
